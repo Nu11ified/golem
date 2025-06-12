b6f113d631e701ac7d4b540460cf3b7b15d934e6
Null <86795298+Nu11ified@users.noreply.github.com>
2025-06-11 22:40:34 -0500

    Update hello.ts

--- user-app/server/ts/hello.ts
+++ user-app/server/ts/hello.ts
@@ -1,4 +1,12 @@
-export default async function handler({ body, query, headers }) {
+export default async function handler({
+  body,
+  query,
+  headers,
+}: {
+  body: Record<string, unknown>;
+  query: Record<string, unknown>;
+  headers: Record<string, string | string[] | undefined>;
+}) {
   return {
     message: "Hello from TypeScript!",
     received: { body, query, headers }
